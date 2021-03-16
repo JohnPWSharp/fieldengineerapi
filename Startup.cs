738ba7970bb414d6190cdb8f9fbbb42a1930873b
Alistair Matthews <alistairwebdojo@live.com>
2021-03-16 09:56:41 +0000

    Configured swagger to start for all deployments

--- Startup.cs
+++ Startup.cs
@@ -43,9 +43,11 @@ namespace FieldEngineerApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FieldEngineerApi v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FieldEngineerApi v1"));
+
 
             app.UseHttpsRedirection();
 
